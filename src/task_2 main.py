7aab95eb7f7d81312c598dd65ca41ade542abebd
Mohammed Afnan <mdafnan@gmail.com>
2026-02-09 07:33:57 +0530

    Day6:task_1.py

---
+++ src/task_2 main.py
@@ -0,0 +1,9 @@
+import math_operations #importing the module
+
+print(math_operations.power(2, 10)) #calling power function
+
+numbers = [10, 20, 30, 40]
+
+print(math_operations.average(numbers)) #calling average function
+
+
